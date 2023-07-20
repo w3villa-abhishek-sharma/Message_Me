160054436dbba56e1d3178230385b3b3bcf86103
w3villa-abhishek-sharma <abhishek.sharma@w3villa.com>
2023-07-20 13:47:02 +0530

    Message Me Application

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'chatroom#index'
+  get 'login', to: 'sessions#new'
+  post 'login' , to: 'sessions#create'
+  delete 'logout' , to: 'sessions#destroy'
+  
+  post 'message', to: 'messages#create'
+ 
+  get 'signup', to: 'users#new'
+    post 'signup', to: 'users#create'
+
+
+  mount ActionCable.server, at: '/cable'
+end
